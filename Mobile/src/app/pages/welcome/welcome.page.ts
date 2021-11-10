7b12ddc85302d9f1f267cb86262a53fda65498cb
Aecooolh1998 <91622246+Aecooolh1998@users.noreply.github.com>
2021-11-10 22:31:53 +0000

    AH - Greet logged in user
    Greeting message for logged in user upon sign in.

--- Mobile/src/app/pages/welcome/welcome.page.ts
+++ Mobile/src/app/pages/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-welcome',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-    constructor() { }
+    public userName = '';
+
+    constructor(
+        private authService: AuthService
+    ) { }
 
     public ngOnInit(): void {
+        this.userName = this.authService.getName();
     }
 
 }
