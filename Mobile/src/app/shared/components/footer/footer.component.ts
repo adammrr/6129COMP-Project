bc470c8d55beac6b8e7eb4e7b53ae21cf10bf600
Aecooolh1998 <91622246+Aecooolh1998@users.noreply.github.com>
2021-11-22 21:26:04 +0000

    AH - Removal of footer modal
    Removed footer modal as mat is now approaching it differently

--- Mobile/src/app/shared/components/footer/footer.component.ts
+++ Mobile/src/app/shared/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { NewRequestComponent } from 'src/app/pages/new-request/new-request.component';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -11,12 +11,4 @@ export class FooterComponent implements OnInit {
   constructor(public modalController: ModalController) {}
 
   ngOnInit() {}
-
-  async openRequestsModal() {
-    const modal = await this.modalController.create({
-      component: NewRequestComponent
-    });
-  
-    return await modal.present();
-    }
 }
