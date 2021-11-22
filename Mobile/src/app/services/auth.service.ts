fdf9e4052c93c5127477a4033e762c8bbd7b78a3
Aecooolh1998 <91622246+Aecooolh1998@users.noreply.github.com>
2021-11-22 21:23:17 +0000

    AH - Logout
    Can log out now on mobile

--- Mobile/src/app/services/auth.service.ts
+++ Mobile/src/app/services/auth.service.ts
@@ -6,56 +6,56 @@ import { AlertService } from './alert.service';
 import { RestService } from './rest.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class AuthService {
 
-    private loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-    private user!: User;
+  private loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private user!: User;
 
-    constructor(
-        private restService: RestService,
-        private alert: AlertService
-    ) { }
+  constructor(
+    private restService: RestService,
+    private alert: AlertService
+  ) { }
 
-    public registerUser(registrationDetails: { firstName: string; surname: string; gender: string; dob: string; address1: string; address2: string; address3: string; email: string; password: string }): void {
-        this.restService.register(registrationDetails).subscribe(async (validationResult: any) => {
-        });
-    }
+  public registerUser(registrationDetails: { firstName: string; surname: string; gender: string; dob: string; address1: string; address2: string; address3: string; email: string; password: string }): void {
+    this.restService.register(registrationDetails).subscribe(async (validationResult: any) => {
+    });
+  }
 
-    public signIn(email: string, password: string): void {
-        this.restService.validateUser(email, password).subscribe(async (validationResult: any) => {
-            if (validationResult.data.result) {
-                console.log('CRUD: Validated');
-                const response = await this.restService.getUserById(validationResult.data.userId).toPromise();
-                this.user = new User(response.data);
-                this.loggedIn.next(true);
-            } else {
-                console.log('CRUD: NOT validated');
-                this.alert.presentToast('Incorrect username or password, please try again');
-            }
-        });
-    }
+  public signIn(email: string, password: string): void {
+    this.restService.validateUser(email, password).subscribe(async (validationResult: any) => {
+      if (validationResult.data.result) {
+        console.log('CRUD: Validated');
+        const response = await this.restService.getUserById(validationResult.data.userId).toPromise();
+        this.user = new User(response.data);
+        this.loggedIn.next(true);
+      } else {
+        console.log('CRUD: NOT validated');
+        this.alert.presentToast('Incorrect username or password, please try again');
+      }
+    });
+  }
 
-    public signOut(): void {
-        this.loggedIn.next(false);
-    }
+  public signOut(): void {
+    this.loggedIn.next(false);
+  }
 
-    public isLoggedIn(): Observable<boolean> {
-        return this.loggedIn;
-    }
+  public isLoggedIn(): Observable<boolean> {
+    return this.loggedIn;
+  }
 
-    public getAccountType(): string {
-        return this.user.accountType;
-    }
+  public getAccountType(): string {
+    return this.user.accountType;
+  }
 
-    public getLoggedInUser() {
-        return this.user;
-    }
+  public getLoggedInUser() {
+    return this.user;
+  }
 
-    public getName(): string {
-        return this.user.firstName + ' ' + this.user.surname;
-    }
+  public getName(): string {
+    return this.user.firstName + ' ' + this.user.surname;
+  }
 
 }
